8fbb957f84816db25c4ac0403ee609a8c3b8a50e
Nicholas Levenson <nicklevenson@gmail.com>
2020-10-27 11:02:05 -0700

    commit tree 6d14eeeccf645ab1c2578fdbc175e9b59f6fecde

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require 'pry'
+
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    attr_accessor :name, :roster
+
+    # def roster
+    #     roster = {}
+    # end
+
+    def add_student(student, grade)
+       if  @roster[grade]
+        @roster[grade] << student
+       else
+        @roster[grade] = []
+        @roster[grade] << student
+       end
+       
+    end
+
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            @roster[grade] = students.sort
+        end
+        
+    end
+        
+end
